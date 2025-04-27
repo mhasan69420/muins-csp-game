56bc29e378fdb6e2012882c3b021c46e103633a5
mhasan69420 <161906685+mhasan69420@users.noreply.github.com>
2025-04-27 16:47:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle as trtl
+import random as rand
+import tkinter
+
+width = 600
+height = 800
+
+apple_size = 30
+bomb_size = 30
+basket_width=100
+basket_hieght=30
+
+fall_speed = 5
+spawn_rate=20
+
+wn=trtl.Screen()
+wn.setup(width,height)
+wn.bgpic("background.gif")
